0eaa46987dca6a907ad53ea54844cb26019653d9
v_ganziy <ganziy.vova@gmail.com>
2018-02-11 23:49:33 +0200

    Добавление функий

--- js/stat.js
+++ js/stat.js
@@ -20,40 +20,53 @@ var COLUMN_WIDTH = 40;
 var COLUMN_HEIGHT = 150;
 var COLUMN_COUNT_GAP = 8;
 var COLUMN_NAME_GAP = 20;
+var FONT = '16px PT Mono';
 
-window.renderStatistics = function (ctx, names, times) {
+var getMaxValue = function (arr) {
   var maxTime = 0;
-  for (var i = 0; i < times.length; i++) {
-    if (maxTime < times[i]) {
-      maxTime = times[i];
+  for (var i = 0; i < arr.length; i++) {
+    if (maxTime < arr[i]) {
+      maxTime = arr[i];
     }
   }
+  return maxTime;
+};
+var getRandomColor = function () {
+  return 'rgba(0, 0, 255, ' + Math.random() + ')';
+};
+var renderText = function (ctx, text, font, color, x, y) {
+  ctx.font = font;
+  ctx.fillStyle = color;
+  ctx.fillText(text, x, y);
+};
+var renderHistogramColumn = function (ctx, x, y, width, height, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+};
 
+window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(START_X + START_GAP, START_Y + START_GAP, START_WIDTH, START_HEIGHT);
 
   ctx.fillStyle = '#fff';
   ctx.fillRect(START_X, START_Y, START_WIDTH, START_HEIGHT);
 
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура, Вы победили!', START_X + TEXT_PADDING, TEXT_Y);
-  ctx.fillText('Список результатов!', START_X + TEXT_PADDING, TEXT_Y + TEXT_GAP);
+  renderText(ctx, 'Ура, Вы победили!', FONT, '#000', START_X + TEXT_PADDING, TEXT_Y);
+  renderText(ctx, 'Список результатов!', FONT, '#000', START_X + TEXT_PADDING, TEXT_Y + TEXT_GAP);
 
   for (var j = 0; j < names.length; j++) {
-    ctx.fillStyle = '#000';
-    var columnHeight = (Math.round(times[j]) * COLUMN_HEIGHT) / maxTime;
+    var columnHeight = (Math.round(times[j]) * COLUMN_HEIGHT) / getMaxValue(times);
     var columnY = COLUMN_Y + (COLUMN_HEIGHT - columnHeight);
 
-    ctx.fillText(names[j], COLUMN_X + j * COLUMN_GAP, COLUMN_Y + COLUMN_HEIGHT + COLUMN_NAME_GAP);
-    ctx.fillText(Math.round(times[j]), COLUMN_X + j * COLUMN_GAP, columnY - COLUMN_COUNT_GAP);
+    renderText(ctx, names[j], FONT, '#000', COLUMN_X + j * COLUMN_GAP, COLUMN_Y + COLUMN_HEIGHT + COLUMN_NAME_GAP);
+    renderText(ctx, Math.round(times[j]), FONT, '#000', COLUMN_X + j * COLUMN_GAP, columnY - COLUMN_COUNT_GAP);
 
-    if (names[i] === 'Вы') {
+    if (names[j] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
+      ctx.fillStyle = getRandomColor();
     }
 
-    ctx.fillRect(COLUMN_X + j * COLUMN_GAP, columnY, COLUMN_WIDTH, columnHeight);
+    renderHistogramColumn(ctx, COLUMN_X + j * COLUMN_GAP, columnY, COLUMN_WIDTH, columnHeight, ctx.fillStyle);
   }
 };
